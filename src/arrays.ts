c2d79554ab649988372217ff2f451144bcb62c98
Sean Hennessey <seanhnsy@udel.edu>
2023-03-03 22:01:01 -0500

    makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -107,7 +107,16 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0
+    );
+    if (addends.length === 0) {
+        return "0=0";
+    } else {
+        const addEmUp = addends.join("+");
+        return sum + "=" + addEmUp;
+    }
 }
 
 /**
