861f09bdd6aa09b72cb2654a38b8499aae5abab6
Sean Hennessey <seanhnsy@udel.edu>
2023-03-07 15:25:18 -0500

    isValid

--- src/objects.ts
+++ src/objects.ts
@@ -49,7 +49,15 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    } else if (question.type === "multiple_choice_question") {
+        if (question.options.includes(answer)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 /**
