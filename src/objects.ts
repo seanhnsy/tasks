1a84c2612bb0d231e5a526b06fb0f14cab74bc31
Sean Hennessey <seanhnsy@udel.edu>
2023-03-07 15:30:48 -0500

    toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -67,7 +67,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const firstTen = question.name.slice(0, 10);
+    return question.id + ": " + firstTen;
 }
 
 /**
