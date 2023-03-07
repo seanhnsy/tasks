60fe450ece74846aa1dbe445b2308905a7047335
Sean Hennessey <seanhnsy@udel.edu>
2023-03-07 15:57:08 -0500

    toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -89,7 +89,15 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "short_answer_question") {
+        return "# " + question.name + "\n" + question.body;
+    } else if (question.type === "multiple_choice_question") {
+        const allAns = question.options.map(
+            (option: string): string => "\n" + "- " + option
+        );
+        const newAllAns = allAns.join("");
+        return "# " + question.name + "\n" + question.body + newAllAns;
+    }
 }
 
 /**
